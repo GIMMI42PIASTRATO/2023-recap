fa2cbeeaf8bd596a2b2bb2cd86e6217e1028b93a
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-12-16 22:40:36 +0100

    Added new fonts

--- src/components/Panel1/Panel1.jsx
+++ src/components/Panel1/Panel1.jsx
@@ -2,13 +2,12 @@ import PropTypes from "prop-types";
 import style from "./Panel1.module.css";
 
 export default function Background({ children }) {
-
-    handleScroll = (event) => {
-        
-    }
+    const handleScroll = () => (event) => {
+        console.log(event);
+    };
 
     return (
-        <div className={style.backgroundContainer} onScroll={() => }>
+        <div className={style.backgroundContainer} onScroll={handleScroll}>
             <div className={style.year}>2023</div>
             <span className={style.title}>{children}</span>
         </div>
