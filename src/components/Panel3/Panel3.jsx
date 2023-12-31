0f6e6477b4df72cd498a41632b62cfbe75708400
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-12-31 10:07:03 +0100

    Created panel3

---
+++ src/components/Panel3/Panel3.jsx
@@ -0,0 +1,9 @@
+import style from "./Panel3.module.css";
+
+export default function Panel3() {
+    return (
+        <div className={style.backgroundContainer}>
+            <div></div>
+        </div>
+    );
+}
